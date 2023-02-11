dc0aad5a4cc3d8c290b6f781dd4448776cdf156b
Alexander Belousov <pegas206@mail.ru>
2023-02-11 11:04:45 +0700

    Builder, начало

---
+++ src/main/java/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    protected String name;
+    protected String surname;
+    protected int age;
+    protected String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
